fdacddbeef9595821ec6f9d0508dd2d835d69a92
DariaOgd <daria.ogd@gmail.com>
2023-12-28 20:36:00 +0100

    update

--- projektPhp/routes/web.php
+++ projektPhp/routes/web.php
@@ -16,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
-Route::get('/login', [\App\Http\Controllers\AuthManager::class, 'login']) -> name('login');
-Route::post('/login', [\App\Http\Controllers\AuthManager::class, 'loginPost']) -> name('login.post');
+Route::get('/', [\App\Http\Controllers\AuthManager::class, 'login']) -> name('login');
+Route::post('/', [\App\Http\Controllers\AuthManager::class, 'loginPost']) -> name('login.post');
 Route::get('/register', [\App\Http\Controllers\AuthManager::class, 'register'])->name('register');
 Route::post('/register', [\App\Http\Controllers\AuthManager::class, 'registerPost']) -> name('register.post');
 Route::get('/logout', [\App\Http\Controllers\AuthManager::class, 'logout'])->name('logout');
